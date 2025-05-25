660b3f1b6e6120ab4a312a5ac1fd85caf9a28c13
omaonyejiakor93 <omaonyejiakor@gmail.com>
2025-05-25 15:02:56 +0100

    Rearranged the output according to ALX instructions

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-const langs = ["C is fun", "JavaScript is amazing", "Python is cool",]
+const langs = ["C is fun", "Python is cool", "JavaScript is amazing",]
 for (let i = 0; i < langs.length; i++) {
     console.log(langs[i]);
     }
